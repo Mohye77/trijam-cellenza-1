8253a410589bb59e2164272f58f8293604f2ad7c
Geoffrey Mohaer <geoffrey.mohaer@athlon.com>
2024-02-16 14:25:35 +0100

    character move animation OK

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,13 @@ var domElements = {
 var usercharWay = [];
 
 function initUI() {
-  domElements.keys = document.querySelectorAll(".key");
-  domElements.keys.forEach((key) => {
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) => {
     const val = getRandomInt(2);
     if (val == 1) {
       key.classList.add("alternate");
     }
+    domElements.keys.push(key);
   });
 
   document.addEventListener("keydown", handleKeydown);
@@ -44,13 +45,9 @@ const keyPath = ["A", "Z", "E", "R", "T", "Y"];
 
 function showPath() {
   keyPath.forEach((char) => {
-    domElements.keys.forEach((key) => {
-      const keychar = key.getAttribute("keychar");
-      if (keychar === char) {
-        key.classList.add("way");
-        domElements.keyWay.push(key);
-      }
-    });
+    const key = getHtmlKey(char);
+    key.classList.add("way");
+    domElements.keyWay.push(key);
   });
 }
 
@@ -89,15 +86,17 @@ function setGoldenIdolPos() {
 function moveCharacter() {
   for (let charIndex = 0; charIndex < usercharWay.length; charIndex++) {
     const userChar = usercharWay[charIndex];
-
-    domElements.keys.forEach((key) => {
-      if (userChar === key.getAttribute("keychar")) {
-        console.log(key);
-        setPosition(domElements.character, getPosition(key));
-        return;
-      }
-    });
+    setTimeout(() => {
+      const key = getHtmlKey(userChar);
+      setPosition(domElements.character, getPosition(key));
+    }, charIndex * 500);
   }
+}
+
+function getHtmlKey(charKey) {
+  return domElements.keys.find(
+    (key) => charKey === key.getAttribute("keychar")
+  );
 }
 
 handleKeydown = function (evt) {
@@ -111,12 +110,13 @@ handleKeydown = function (evt) {
       setTimeout(() => {
         var userWay = prompt("Show me the way! (Use CAPS LOCK)");
         usercharWay = Array.from(userWay);
-        for (let charIndex = 0; charIndex < usercharWay.length; charIndex++) {
-          const userChar = usercharWay[charIndex];
-          if (charIndex > keyPath.length - 1) {
-            if (userChar !== keyPath[charIndex]) {
+        if (usercharWay.length != keyPath.length) {
+          gameState.error = true;
+        } else {
+          for (let charIndex = 0; charIndex < usercharWay.length; charIndex++) {
+            if (usercharWay[charIndex] !== keyPath[charIndex]) {
               gameState.error = true;
-              return;
+              break;
             }
           }
         }
@@ -127,7 +127,7 @@ handleKeydown = function (evt) {
         } else {
           domElements.success.style.display = "block";
         }
-      }, 1000);
+      }, 500);
       break;
     default:
       break;
